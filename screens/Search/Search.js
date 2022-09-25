b8ea24e01840b18cf41e94dcc3e496bb63135596
nascript <nascodefy@gmail.com>
2022-09-25 22:14:58 +0700

    make sure fix

--- screens/Search/Search.js
+++ screens/Search/Search.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React from 'react'
 import { View, Text } from 'react-native'
 
 const Search = () => {
-    return (
-      <View
-        style={{
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <Text>Notification</Text>
-      </View>
-    )
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Text>Search</Text>
+    </View>
+  )
 }
 
 export default Search
