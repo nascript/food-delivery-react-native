a975cb2d4fe8e07145dedadb129ad6f1c38639b0
nascript <nascodefy@gmail.com>
2022-09-27 21:33:56 +0700

    onBoarding Screen

--- screens/OnBoarding/OnBoarding.js
+++ screens/OnBoarding/OnBoarding.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { View, Text, ImageBackground, Image, Animated } from 'react-native'
 import { constants, images, FONTS, SIZES, COLORS } from '../../constants'
+import { TextButton } from '../../components'
 
-const OnBoarding = () => {
+const OnBoarding = ({ navigation }) => {
+  const scrolX = useRef(new Animated.Value(0)).current
+  const flatListRef = useRef(0)
+
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const onViewChangeRef = useRef(({ viewableItems, changed }) => {
+    setCurrentIndex(viewableItems[0].index)
+  })
+
   function renderHeaderLogo() {
     return (
       <View
@@ -23,6 +33,110 @@ const OnBoarding = () => {
       </View>
     )
   }
+
+  const Dots = () => {
+    const dotPosition = Animated.divide(scrolX, SIZES.width)
+
+    return (
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        {constants.onboarding_screens.map((item, index) => {
+          const dotColor = dotPosition.interpolate({
+            inputRange: [index - 1, index, index + 1],
+            outputRange: [
+              COLORS.lightOrange,
+              COLORS.primary,
+              COLORS.lightOrange,
+            ],
+            extrapolate: 'clamp',
+          })
+
+          const dotWidth = dotPosition.interpolate({
+            inputRange: [index - 1, index, index + 1],
+            outputRange: [10, 30, 10],
+            extrapolate: 'clamp',
+          })
+
+          return (
+            <Animated.View
+              key={`dot-${index}`}
+              style={{
+                borderRadius: 5,
+                marginHorizontal: 6,
+                width: dotWidth,
+                height: 10,
+                backgroundColor: dotColor,
+              }}
+            />
+          )
+        })}
+      </View>
+    )
+  }
+
+  function renderFooter() {
+    return (
+      <View style={{ height: 160, width: '100%' }}>
+        {/* Paginations / Dots */}
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <Dots />
+        </View>
+
+        {/* Buttons */}
+        {currentIndex < constants.onboarding_screens.length - 1 && (
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              paddingHorizontal: SIZES.padding,
+              marginVertical: SIZES.padding,
+            }}
+          >
+            <TextButton
+              label='Skip'
+              buttonContainerStyle={{ backgroundColor: null }}
+              labelStyle={{ color: COLORS.darkGray2 }}
+              onPress={() => navigation.replace('SignIn')}
+            />
+            <TextButton
+              label='Next'
+              buttonContainerStyle={{
+                height: 60,
+                width: 200,
+                borderRadius: SIZES.radius,
+              }}
+              onPress={() => {
+                flatListRef?.current?.scrollToIndex({
+                  index: currentIndex + 1,
+                  animated: true,
+                })
+              }}
+            />
+          </View>
+        )}
+        {currentIndex == constants.onboarding_screens.length - 1 && (
+          <View
+            style={{
+              paddingHorizontal: SIZES.padding,
+              marginVertical: SIZES.padding,
+            }}
+          >
+            <TextButton
+              label="Let's Get Started"
+              buttonContainerStyle={{ height: 60, borderRadius: SIZES.radius }}
+              onPress={() => navigation.replace('SignIn')}
+            />
+          </View>
+        )}
+      </View>
+    )
+  }
   return (
     <View
       style={{
@@ -36,12 +150,18 @@ const OnBoarding = () => {
 
       {/* Content Splashscreen */}
       <Animated.FlatList
+        ref={flatListRef}
         horizontal
         pagingEnabled
         data={constants.onboarding_screens}
         scrollEventThrottle={16}
         snapToAlligment='center'
         showHorizontalScrollIndicator={false}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrolX } } }],
+          { useNativeDriver: false }
+        )}
+        onViewableItemsChanged={onViewChangeRef.current}
         keyExtractor={(item) => `${item.id}`}
         renderItem={({ item, index }) => {
           return (
@@ -54,7 +174,7 @@ const OnBoarding = () => {
                     flex: 1,
                     alignItems: 'center',
                     justifyContent: 'flex-end',
-                    height: '100%',
+                    height: index == 1 ? '92%' : '100%',
                     width: '100%',
                   }}
                 >
@@ -96,9 +216,9 @@ const OnBoarding = () => {
             </View>
           )
         }}
-          />
-          
-          {renderFooter()}
+      />
+
+      {renderFooter()}
     </View>
   )
 }
